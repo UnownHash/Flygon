2fb4807857d39b91b950aff923c973c280ad0e5b
Fabio1988 <fabio.schett@gmail.com>
2023-03-29 19:28:20 +0200

    fix config

--- config/config.go
+++ config/config.go
@@ -1,63 +1,63 @@
 package config
 
 type configDefinition struct {
-	General    generalDefinition   `toml:"general"`
-	Processors processorDefinition `toml:"processors"`
-	Worker     workerDefinition    `toml:"worker"`
-	Db         DbDefinition        `toml:"db"`
-	Sentry     sentry              `toml:"sentry"`
-	Pyroscope  pyroscope           `toml:"pyroscope"`
+	General    generalDefinition   `mapstructure:"general"`
+	Processors processorDefinition `mapstructure:"processors"`
+	Worker     workerDefinition    `mapstructure:"worker"`
+	Db         DbDefinition        `mapstructure:"db"`
+	Sentry     sentry              `mapstructure:"sentry"`
+	Pyroscope  pyroscope           `mapstructure:"pyroscope"`
 }
 
 type generalDefinition struct {
-	WorkerStatsInterval int    `toml:"worker_stats_interval"`
-	SaveLogs            bool   `toml:"save_logs"`
-	DebugLogging        bool   `toml:"debug_log"`
-	Host                string `toml:"host"`
-	Port                int    `toml:"port"`
-	ApiSecret           string `tom:"api_secret"`
-	BearerToken         string `tom:"bearer_token"`
-	RouteCalcUrl        string `toml:"routecalc_url"`
-	KojiUrl             string `toml:"koji_url"`
-	KojiBearerToken     string `toml:"koji_bearer_token"`
+	WorkerStatsInterval int    `mapstructure:"worker_stats_interval"`
+	SaveLogs            bool   `mapstructure:"save_logs"`
+	DebugLogging        bool   `mapstructure:"debug_log"`
+	Host                string `mapstructure:"host"`
+	Port                int    `mapstructure:"port"`
+	ApiSecret           string `mapstructure:"api_secret"`
+	BearerToken         string `mapstructure:"bearer_token"`
+	RouteCalcUrl        string `mapstructure:"routecalc_url"`
+	KojiUrl             string `mapstructure:"koji_url"`
+	KojiBearerToken     string `mapstructure:"koji_bearer_token"`
 }
 
 type processorDefinition struct {
-	GolbatEndpoint  string   `toml:"golbat_endpoint"`
-	GolbatRawBearer string   `toml:"golbat_raw_bearer"`
-	GolbatApiSecret string   `toml:"golbat_api_secret"`
-	RawEndpoints    []string `toml:"raw_endpoints"`
+	GolbatEndpoint  string   `mapstructure:"golbat_endpoint"`
+	GolbatRawBearer string   `mapstructure:"golbat_raw_bearer"`
+	GolbatApiSecret string   `mapstructure:"golbat_api_secret"`
+	RawEndpoints    []string `mapstructure:"raw_endpoints"`
 }
 
 type workerDefinition struct {
-	LoginDelay       int `toml:"login_delay"`
-	RoutePartTimeout int `toml:"route_part_timeout"`
+	LoginDelay       int `mapstructure:"login_delay"`
+	RoutePartTimeout int `mapstructure:"route_part_timeout"`
 }
 
 type DbDefinition struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	Name     string
-	MaxPool  int `toml:"max_pool"`
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Name     string `mapstructure:"name"`
+	MaxPool  int    `mapstructure:"max_pool"`
 }
 
 type sentry struct {
-	DSN              string  `toml:"dsn"`
-	Debug            bool    `toml:"debug"`
-	SampleRate       float64 `toml:"sample_rate"`
-	EnableTracing    bool    `toml:"enable_tracing"`
-	TracesSampleRate float64 `toml:"traces_sample_rate"`
+	DSN              string  `mapstructure:"dsn"`
+	Debug            bool    `mapstructure:"debug"`
+	SampleRate       float64 `mapstructure:"sample_rate"`
+	EnableTracing    bool    `mapstructure:"enable_tracing"`
+	TracesSampleRate float64 `mapstructure:"traces_sample_rate"`
 }
 
 type pyroscope struct {
-	ApplicationName      string `toml:"application_name"`
-	ServerAddress        string `toml:"server_address"`
-	ApiKey               string `toml:"api_key"`
-	Logger               bool   `toml:"logger"`
-	MutexProfileFraction int    `toml:"mutex_profile_fraction"`
-	BlockProfileRate     int    `toml:"block_profile_rate"`
+	ApplicationName      string `mapstructure:"application_name"`
+	ServerAddress        string `mapstructure:"server_address"`
+	ApiKey               string `mapstructure:"api_key"`
+	Logger               bool   `mapstructure:"logger"`
+	MutexProfileFraction int    `mapstructure:"mutex_profile_fraction"`
+	BlockProfileRate     int    `mapstructure:"block_profile_rate"`
 }
 
 var Config configDefinition
