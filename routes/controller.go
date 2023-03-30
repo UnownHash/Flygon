9adcd88df9c812f680dfe5085f97bca5e85bc00a
Fabio1988 <35898099+Fabio1988@users.noreply.github.com>
2023-03-30 16:54:45 +0200

    fix login delay response

--- routes/controller.go
+++ routes/controller.go
@@ -142,7 +142,10 @@ func handleGetAccount(c *gin.Context, req ControllerBody, workerState *worker.St
 			if remainingTime := value.(int64) + int64(loginDelay) - now; remainingTime > 0 {
 				log.Debugf("[CONTROLLER] [%s] Login for host %s throttled, remaining %d s", req.Uuid, host, remainingTime)
 				c.Header("Retry-After", strconv.FormatInt(remainingTime, 10))
-				respondWithError(c, LoginLimitExceeded)
+				c.JSON(http.StatusTooManyRequests, gin.H{
+					"status": "error",
+					"error":  LoginLimitExceeded.String(),
+				})
 				return
 			}
 		}
